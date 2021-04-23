dbd9aa90e72d98dc094d58a878804bad9a8acc13
Maxsuel Fernando <maxsuel.dev@gmail.com>
2021-04-22 21:40:50 -0300

    update repo

---
+++ index.php
@@ -0,0 +1,264 @@
+<!doctype html>
+<html lang="pt-br" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="description" content="Maxsuel Fernando - Analista de Sistemas, Desenvolvedor e Programador PHP">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Maxsuel Fernando - Portfólio</title>
+
+	<!-- <link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/grid.css"> -->
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<!-- <link rel="stylesheet" href="css/style-raw.css?=<?php print time(); ?>">
+	<link rel="stylesheet" href="css/responsivo.css?=<?php print time(); ?>"> -->
+	
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/libs/modernizr-custom.js"></script>
+</head>
+<body>
+
+	<section id="intro" class="intro">	
+		<div class="container16 content-intro">
+			<h1>Maxsuel Fernando</h1>
+			<div class="intro-efect">
+				<h3 class="anim-typewriter">Analista de Sistemas | Web Designer | Programador PHP</h3>
+			</div>
+			<div class="redes-sociais">
+				<a href="https://www.facebook.com/maxsuelfernandob" target="_blank">
+					<img src="img/redes-sociais/facebook-logo.png" alt="facebook-logo" class="social-logo">
+				</a>
+				<a href="https://www.linkedin.com/in/maxsuelfernandob" target="_blank">
+					<img src="img/redes-sociais/linkedin-logo.png" alt="linkedin-logo" class="social-logo">
+				</a>
+				<a href="https://github.com/maxsuelfernandob" target="_blank">
+					<img src="img/redes-sociais/github-logo.png" alt="github-logo" class="social-logo">
+				</a>
+			</div>
+			<a href="#contato" class="call-contato">Entrar em contato</a>
+		</div>
+		<a class="mouse" href="#sobre">
+			<div class="scroll-downs">
+  				<div class="mousey">
+   					<div class="scroller"></div>
+  				</div>
+			</div>
+		</a>
+	</section>
+
+	<section id="sobre" class="sobre">
+		<div class="container16">
+			<div class="sc_head">
+				<i class="fa fa-user fa-3x icon_section" aria-hidden="true"></i>
+				<h2 class="title_section">Sobre</h2>
+				<span class="clear"></span>
+				<h3>Conheça um pouco sobre mim</h3>
+			</div>
+
+			<hr class="separador sl">
+			
+			<div class="column13">
+				
+				<h3 class="h3_bold">Maxsuel Fernando</h3>
+				<h3 class="h3_bold">Analista de Sistemas | Web Designer | Programador PHP</h3>
+
+				<div class="texto">
+					<p>Graduado em Sistemas de Informação, com maiores habilidades Web Design, Desenvolvimento Web e Suporte Técnico, venho buscando sempre novos conhecimentos na área, pois, tenho interesse em evoluir profissionalmente, com o objetivo de ascensão profissional.</p>
+				</div>
+				<div>
+					<h3 class="h3_bold">(82) 99912-7655</h3>
+					<h3 class="h3_bold">Maceió - AL</h3>
+				</div>
+			</div>
+			<div class="column3 maxsuel_img">
+				<img class="maxsuelfernando" src="img/sobre/maxsuel_fernando.jpg" alt="Maxsuel Fernando">
+			</div>
+		</div>
+	</section>
+
+	<section class="skills">
+		<div class="container16">
+			<div class="sc_head">
+				<i class="fa fa-shield fa-3x icon_section" aria-hidden="true"></i>
+				<h2 class="title_section">Skills</h2>
+				<span class="clear"></span>
+				<h3>Ferramentas que utilizo em meus projetos</h3>
+			</div>
+
+			<hr class="separador sb">
+		
+		</div>
+
+		<div class="container12 text-center">
+			<div class="column3">
+				<img class="php" src="img/skills/php.png" alt="Php" width="114px" height="61px">
+			</div>
+			<div class="column3">
+				<img class="html5" src="img/skills/html5.png" alt="Html 5" width="50px" height="60px">
+			</div>
+			<div class="column3">
+				<img class="css3" src="img/skills/css3.png" alt="Css 3" width="68px" height="60px">
+			</div>
+			<div class="column3">
+				<img class="bootstrap" src="img/skills/bootstrap.png" alt="Bootstrap" width="60px" height="60px">
+			</div>
+
+			<div class="column4">
+				<img class="angular" src="img/skills/angular.png" alt="Angular" width="54px" height="59px">
+			</div>
+			<div class="column4">
+				<img class="javascript" src="img/skills/javascript.png" alt="JavaScript" width="60px" height="60px">
+			</div>
+			<div class="column4">
+				<img class="database" src="img/skills/database.png" alt="Database" width="61px" height="61px">
+			</div>
+		</div>
+	</section>
+
+	<section class="servicos">
+		<div class="container16">
+			<div class="sc_head">
+				<i class="fa fa-cog fa-3x icon_section" aria-hidden="true"></i>
+				<h2 class="title_section">Serviços</h2>
+				<span class="clear"></span>
+				<h3>Serviços oferecidos para o desenvolvimento de sua marca</h3>
+			</div>
+
+			<hr class="separador sl">
+		</div>
+		<div class="container12 text-center">
+			<div class="column4">
+				<img class="servico01" src="img/servicos/sites.png" alt="serviço_01" width="215px" height="142px">
+				<h3><strong>SITES</strong></h3>
+				<p>Desenvolvimento de Sites</p>
+			</div>
+			<div class="column4">
+				<img class="servico02" src="img/servicos/sistemas.png" alt="serviço_02" width="150px" height="111px">
+				<h3><strong>SISTEMAS</strong></h3>
+				<p>Desenvolvimento de Sistemas</p>
+			</div>
+			<div class="column4">
+				<img class="servico03" src="img/servicos/loja_virtual.png" alt="serviço_03" width="167px" height="63px">
+				<h3><strong>LOJA VIRTUAL</strong></h3>
+				<p>Desenvolvimento de Comércio Eletrônico</p>
+			</div>
+		</div>
+	</section>
+
+	<section class="portfolio">
+		<div class="container16">
+			<div class="sc_head">
+				<i class="fa fa-code fa-3x icon_section" aria-hidden="true"></i>
+				<h2 class="title_section">Portfólio Web Designer</h2>
+				<span class="clear"></span>
+				<h3>Projetos de sites que desenvolvi</h3>
+			</div>
+
+			<hr class="separador sb">
+		</div>	
+		<div class="container12 text-center">
+			<div class="pjt_reais">
+				<h3 class="pjt_title">Projetos reais</h3>
+				<div class="column4">
+					<a href="portfolio/escolacreative" target="_blank">
+						<img src="img/portfolio/site01.png" alt="Site 01">
+					</a>
+				</div>
+				<div class="column4">
+					<a href="portfolio/vamos-mudar-o-mundo" target="_blank">
+						<img src="img/portfolio/site02.png" alt="Site 02">
+					</a>
+				</div>
+				<div class="column4">
+					<a href="portfolio/vamos-mudar-o-mundo/blog" target="_blank">
+						<img src="img/portfolio/site03.png" alt="Site 03">
+					</a>
+				</div>
+			</div>
+	
+			<span class="clear"></span>
+
+			<div class="templates">
+				<h3 class="pjt_title">Templates</h3>
+				<div class="column3">
+					<a href="portfolio/site1" target="_blank">
+						<img src="img/portfolio/site04.png" alt="Site 04">
+					</a>
+				</div>
+				<div class="column3">
+					<a href="portfolio/site2" target="_blank">
+						<img src="img/portfolio/site05.png" alt="Site 05">
+					</a>
+				</div>
+				<div class="column3">
+					<a href="portfolio/site3" target="_blank">
+						<img src="img/portfolio/site06.png" alt="Site 06">
+					</a>
+				</div>
+				<div class="column3">
+					<a href="portfolio/site4" target="_blank">
+						<img src="img/portfolio/site07.png" alt="Site 07">
+					</a>
+				</div>
+			</div>
+		</div>		
+	</section>
+
+	<section id="contato" class="contato">
+		<div class="container16">
+			<div class="sc_head">
+				<i class="fa fa-envelope fa-3x icon_section" aria-hidden="true"></i>
+				<h2 class="title_section">Contato</h2>
+				<span class="clear"></span>
+				<h3>Entre em contato para solicitar um orçamento ou tirar dúvidas </h3>
+			</div>
+
+			<hr class="separador sl">
+
+			<form id="form_contato" action="" method="post" name="form" class="formphp form" onsubmit="enviar.disabled=true;">
+				
+				<label class="nao-aparece">Se você não é um robô, deixe em branco.</label>
+				<input type="text" class="nao-aparece" name="leaveblank" value="">
+				<label class="nao-aparece">Se você não é um robô, não mude este campo.</label>
+				<input type="text" class="nao-aparece" name="dontchange" value="http://">
+
+				<div class="column8">
+					<input type="text" name="nome" placeholder="Nome" required>
+					<input type="email" name="email" placeholder="Email" required>
+				</div>
+				<div class="column8">
+					<textarea rows="3" name="mensagem" placeholder="Mensagem" required></textarea>
+				</div>
+				<span class="clear"></span>
+				<div class="button-form">
+					<button class="btn direciona" name="enviar" type="submit">Enviar</button>
+				</div>
+			</form>
+		</div>
+	</section>
+
+	<footer class="footer">
+		<div class="container16 text-center">
+			<a href="https://www.facebook.com/maxsuelfernandob" target="_blank">
+				<img src="img/redes-sociais/facebook-logo.png" alt="facebook-logo" class="social-logo">
+			</a>
+			<a href="https://www.linkedin.com/in/maxsuelfernandob" target="_blank">
+				<img src="img/redes-sociais/linkedin-logo.png" alt="linkedin-logo" class="social-logo">
+			</a>
+			<a href="https://github.com/maxsuelfernandob" target="_blank">
+				<img src="img/redes-sociais/github-logo.png" alt="github-logo" class="social-logo">
+			</a>
+			
+			<!-- <div class="info">
+				<p>(82) 99912-7655</p>
+				<p>maxsuelf94@gmail.com</p>
+			</div> -->
+		</div>
+	</footer>
+
+	<script src="js/libs/jquery-1.9.1.min.js"></script>
+	<script src="js/libs/visibility.js"></script>
+	<script src="js/script.min.js?=<?php print time(); ?>"></script>
+
+</body>
+</html>
